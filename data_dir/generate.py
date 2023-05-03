94bb0c1ed367407db6c4da686d8277084cbd4f48
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2023-05-03 11:22:41 +0400

    Added generate.py

---
+++ data_dir/generate.py
@@ -0,0 +1,6 @@
+import datetime as dt
+import yfinance as yf 
+#fetching previous stock data for Apple and Microsoft
+stock_data = yf.download(['AAPL','MSFT'],dt.datetime(2001,1,1),dt.datetime(2023,1,1)) 
+#saving the time series stock data information to a csv file to be fed to Kafka 
+stock_data.to_csv("data_dir/stocks.csv",index=False)
